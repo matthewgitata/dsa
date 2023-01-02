7eaad7b5b8d5d8f89b680d336b022d199be31453
Matthew Gitata <matthewgitata@gmail.com>
2023-01-02 11:07:00 +0300

    Insert elements into a 2D Array.

--- Arrays/Two Dimensional Array/src/com/matthewgitata/dsa/arrays/tda/Main.java
+++ Arrays/Two Dimensional Array/src/com/matthewgitata/dsa/arrays/tda/Main.java
@@ -8,20 +8,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // Declare
-        int[][] int2DArray;
-        // Instantiate
-        int2DArray = new int[2][2];
-        //Initialize
-        int2DArray[0][0] = 1;
-        int2DArray[0][1] = 2;
-        int2DArray[1][0] = 3;
-        int2DArray[1][1] = 4;
-
-        System.out.println(Arrays.deepToString(int2DArray));
-
-        // All Together
-        String[][] s2DArray = {{"a", "b"}, {"c", "d"}};
-        System.out.println(Arrays.deepToString(s2DArray));
+        TwoDimensionalArray tda = new TwoDimensionalArray(3, 3);
+        tda.insertValueInTheArray(0, 0, 3);
+        tda.insertValueInTheArray(0, 0, 3);
+        System.out.println(Arrays.deepToString(tda.arr));
     }
 }
---
+++ Arrays/Two Dimensional Array/src/com/matthewgitata/dsa/arrays/tda/TwoDimensionalArray.java
@@ -0,0 +1,34 @@
+package com.matthewgitata.dsa.arrays.tda;
+
+/**
+ * created by @matthewgitata on 02/10/2023
+ */
+
+public class TwoDimensionalArray {
+    int[][] arr = null;
+
+    // Constructor
+    public TwoDimensionalArray(int numberOfRows, int numberOfColumns) {
+        this.arr = new int[numberOfRows][numberOfColumns];
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[0].length; column++) {
+                arr[row][column] = Integer.MIN_VALUE;
+            }
+        }
+    }
+
+    // Inserting value in the Array
+    public void insertValueInTheArray(int row, int col, int value) {
+        try {
+            if (arr[row][col] == Integer.MIN_VALUE) {
+                arr[row][col] = value;
+                System.out.println("The value is successfully inserted.");
+            } else {
+                System.out.println("This cell is already occupied.");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index for 2D array.");
+        }
+    }
+}
