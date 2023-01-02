34a34ee52de36331f3c970d9313ecce1277bda4d
Matthew Gitata <matthewgitata@gmail.com>
2023-01-02 07:36:00 +0300

    Access an element in array.

--- Arrays/ArrayInsertion/src/com/matthewgitata/dsa/arrays/arrayinsertion/Main.java
+++ Arrays/ArrayInsertion/src/com/matthewgitata/dsa/arrays/arrayinsertion/Main.java
@@ -6,10 +6,21 @@ package com.matthewgitata.dsa.arrays.arrayinsertion;
 
 public class Main {
     public static void main(String[] args) {
+        // Create SDA Instance
         SingleDimensionalArray sda = new SingleDimensionalArray(10);
+
+        // Insert elements into the array
+        sda.insert(0, 28);
         sda.insert(3, 8);
         sda.insert(5, 7);
         sda.insert(5, 5);
         sda.insert(12, 120);
+
+        // Access array elements
+        System.out.println("\nArray access");
+        var firstElement = sda.arr[0];
+        System.out.println("First element: " + firstElement);
+        var fourthElement = sda.arr[3];
+        System.out.println("Fourth element: " + fourthElement);
     }
 }
