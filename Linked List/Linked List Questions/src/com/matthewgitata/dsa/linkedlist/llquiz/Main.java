5ecf58355419b62fae6e19f782848d90827194b7
Matthew Gitata <matthewgitata@gmail.com>
2023-01-22 19:18:00 +0300

    Partition elements of a LL around a given number.

--- Linked List/Linked List Questions/src/com/matthewgitata/dsa/linkedlist/llquiz/Main.java
+++ Linked List/Linked List Questions/src/com/matthewgitata/dsa/linkedlist/llquiz/Main.java
@@ -10,13 +10,13 @@ public class Main {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.createLL(1);
-        ll.insertNode(2);
-        ll.insertNode(2);
-        ll.insertNode(3);
+        ll.insertNode(9);
         ll.insertNode(5);
+        ll.insertNode(10);
+        ll.insertNode(2);
         ll.traversalLL();
         Questions q = new Questions();
-        Node n = q.nthToLast(ll, 2);
-        System.out.println(n.value);
+        LinkedList t = q.partition(ll, 4);
+        t.traversalLL();
     }
 }
--- Linked List/Linked List Questions/src/com/matthewgitata/dsa/linkedlist/llquiz/Questions.java
+++ Linked List/Linked List Questions/src/com/matthewgitata/dsa/linkedlist/llquiz/Questions.java
@@ -47,4 +47,25 @@ public class Questions {
         }
         return p1;
     }
+
+    /**
+     * Partitions a LL around a value x.
+     */
+    LinkedList partition(LinkedList ll, int x) {
+        Node currentNode = ll.head;
+        ll.tail = ll.head;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            if (currentNode.value < x) {
+                currentNode.next = ll.head;
+                ll.head = currentNode;
+            } else {
+                ll.tail.next = currentNode;
+                ll.tail = currentNode;
+            }
+            currentNode = next;
+        }
+        ll.tail.next = null;
+        return ll;
+    }
 }
