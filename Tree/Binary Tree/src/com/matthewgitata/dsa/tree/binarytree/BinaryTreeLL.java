845a070c59e527dff393cf16fbc2b3c85f75506c
Matthew Gitata <matthewgitata@gmail.com>
2023-01-31 21:26:00 +0300

    Create Binary Tree using Linked List.

---
+++ Tree/Binary Tree/src/com/matthewgitata/dsa/tree/binarytree/BinaryTreeLL.java
@@ -0,0 +1,19 @@
+package com.matthewgitata.dsa.tree.binarytree;
+
+/**
+ * THe {@code BinaryTreeLL} class defines the properties of a BinaryTreeLL
+ * and the accompanying operations including create, insert, delete,
+ * search, traverse, and deleteTree.
+ * <p>
+ * created by @matthewgitata on 31/01/2023
+ */
+public class BinaryTreeLL {
+    BinaryNode root;
+
+    /**
+     * Create a BinaryTreeLL.
+     */
+    public BinaryTreeLL() {
+        this.root = null;
+    }
+}
---
+++ Tree/Binary Tree/src/com/matthewgitata/dsa/tree/binarytree/Main.java
@@ -0,0 +1,13 @@
+package com.matthewgitata.dsa.tree.binarytree;
+
+/**
+ * The {@code Main} class contains method where all operations on a
+ * BinaryTree are performed.
+ * <p>
+ * created by @matthewgitata on 31/01/2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        BinaryTreeLL binaryTree = new BinaryTreeLL();
+    }
+}
