8262c07554b0cbf19489d9c1ac5f763a8f22462b
Matthew Gitata <matthewgitata@gmail.com>
2023-01-31 04:40:00 +0300

    Implement Queue Operations using Linked List: create, isEmpty, and enQueue.

---
+++ Queue/QueueLinkedList/src/com/matthewgitata/dsa/queue/queuelinkedlist/Main.java
@@ -0,0 +1,24 @@
+package com.matthewgitata.dsa.queue.queuelinkedlist;
+
+/**
+ * The {@code Main} class constitutes method where all operations
+ * performed on a Queue are done.
+ * <p>
+ * created by @matthewgitata on 31/01/2023
+ */
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Queue creation.");
+        QueueLinkedList newQLL = new QueueLinkedList();
+        System.out.println("Is Queue empty?");
+        boolean isEmpty = newQLL.isEmpty();
+        System.out.println(isEmpty);
+        System.out.println("EnQueueing");
+        newQLL.enQueue(10);
+        newQLL.enQueue(20);
+        newQLL.enQueue(30);
+        System.out.println("Is Queue empty?");
+        boolean isEmpty2 = newQLL.isEmpty();
+        System.out.println(isEmpty2);
+    }
+}
