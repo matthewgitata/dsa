391594c326efb261f9ed569472554f39f7f3fd4f
Matthew Gitata <matthewgitata@gmail.com>
2023-02-01 11:03:00 +0300

    Insert Method Binary Tree (Array).

--- Tree/Binary Tree Arr/src/com/matthewgitata/dsa/tree/binarytreearr/BinaryTree.java
+++ Tree/Binary Tree Arr/src/com/matthewgitata/dsa/tree/binarytreearr/BinaryTree.java
@@ -21,4 +21,32 @@ public class BinaryTree {
         lastUsedIndex = 0;
         System.out.println("Blank Tree of size " + size + " has been created.");
     }
+
+    /**
+     * Checks if Binary Tree is full.
+     *
+     * @return true if BT is full, otherwise false.
+     */
+    public boolean isFull() {
+        if (lastUsedIndex == arr.length - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Inserts node into the BinaryTree.
+     *
+     * @param value node value to be inserted.
+     */
+    public void insert(String value) {
+        if (!isFull()) {
+            arr[lastUsedIndex + 1] = value;
+            lastUsedIndex++;
+            System.out.println("The value of " + value + " has been successfully inserted.");
+        } else {
+            System.out.println("The Binary Tree is Full!");
+        }
+    }
 }
--- Tree/Binary Tree Arr/src/com/matthewgitata/dsa/tree/binarytreearr/Main.java
+++ Tree/Binary Tree Arr/src/com/matthewgitata/dsa/tree/binarytreearr/Main.java
@@ -10,5 +10,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("BT Creation");
         BinaryTree newBinaryTree = new BinaryTree(5);
+        System.out.println("BT Insertion");
+        newBinaryTree.insert("A1");
+        newBinaryTree.insert("A2");
+        newBinaryTree.insert("A3");
+        newBinaryTree.insert("A4");
+        newBinaryTree.insert("A5");
+        newBinaryTree.insert("A6");
     }
 }
