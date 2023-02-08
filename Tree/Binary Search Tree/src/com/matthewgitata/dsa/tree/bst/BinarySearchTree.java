1e4c779b22a39754b053f213f4196fd9cbbf0dc3
Matthew Gitata <matthewgitata@gmail.com>
2023-02-08 04:38:00 +0300

    Create Binary Search Tree.

---
+++ Tree/Binary Search Tree/src/com/matthewgitata/dsa/tree/bst/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package com.matthewgitata.dsa.tree.bst;
+
+/**
+ * The {@code BinarySearchTree} class defines the properties of a
+ * BinarySearchTree object as well as the accompanying functions that
+ * can be performed on it including creation, insertion, node deletion,
+ * search, traversal and BST deletion.
+ * <p>
+ * created by @matthewgitata on 08/02/2023.
+ */
+public class BinarySearchTree {
+    BinaryNode root;
+
+    /**
+     * Initiates root; Creates BinarySearchTree, BST.
+     */
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+}
---
+++ Tree/Binary Search Tree/src/com/matthewgitata/dsa/tree/bst/Main.java
@@ -0,0 +1,13 @@
+package com.matthewgitata.dsa.tree.bst;
+
+/**
+ * The {@code Main} class contains method where all BST operations can be
+ * performed.
+ * <p>
+ * created by @matthewgitata on 07/02/2023.
+ */
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree newBST = new BinarySearchTree();
+    }
+}
