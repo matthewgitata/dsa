f8fc7013b6d7de576fa3644ba6b167797925c7b3
Matthew Gitata <matthewgitata@gmail.com>
2023-02-08 05:25:00 +0300

    Insert a Node to Binary Search Tree.

--- Tree/Binary Search Tree/src/com/matthewgitata/dsa/tree/bst/BinarySearchTree.java
+++ Tree/Binary Search Tree/src/com/matthewgitata/dsa/tree/bst/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package com.matthewgitata.dsa.tree.bst;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * The {@code BinarySearchTree} class defines the properties of a
  * BinarySearchTree object as well as the accompanying functions that
@@ -18,4 +21,34 @@ public class BinarySearchTree {
         this.root = null;
     }
 
+    /**
+     * Inserts a node to the BST.
+     *
+     * @param currentNode the current node.
+     * @param value     node value to be inserted.
+     * @return the root node.
+     */
+    private BinaryNode insert(BinaryNode currentNode, int value) {
+        if (currentNode == null) {
+            BinaryNode newNode = new BinaryNode();
+            newNode.value = value;
+            System.out.println("The value successfully inserted.");
+            return newNode;
+        } else if (value <= currentNode.value) {
+            currentNode.left = insert(currentNode.left, value);
+            return currentNode;
+        } else {
+            currentNode.right = insert(currentNode.right, value);
+            return currentNode;
+
+        }
+    }
+
+    /**
+     * Insert root node.
+     * @param value value of node to be inserted.
+     */
+    void insert(int value) {
+        insert(this.root, value);
+    }
 }
