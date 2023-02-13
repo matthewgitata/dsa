a285cc08ad2c96d443854c0da7351b0058acb75a
Matthew Gitata <matthewgitata@gmail.com>
2023-02-13 09:42:00 +0300

    Implement Binary Search.

--- Searching/Linear Search/src/com/matthewgitata/dsa/search/linearsearch/Main.java
+++ Searching/Linear Search/src/com/matthewgitata/dsa/search/linearsearch/Main.java
@@ -9,7 +9,12 @@ package com.matthewgitata.dsa.search.linearsearch;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 10, 23, 11};
+        int[] arr2 = {8, 9, 12, 15, 17, 19, 20, 21, 28};
+        System.out.println("Linear Search");
         Searching.linearSearch(arr, 10);
         Searching.linearSearch(arr, 17);
+        System.out.println("\nBinary Search");
+        Searching.binarySearch(arr2, 17);
+        Searching.binarySearch(arr2, 30);
     }
 }
--- Searching/Linear Search/src/com/matthewgitata/dsa/search/linearsearch/Searching.java
+++ Searching/Linear Search/src/com/matthewgitata/dsa/search/linearsearch/Searching.java
@@ -9,7 +9,8 @@ package com.matthewgitata.dsa.search.linearsearch;
 public class Searching {
 
     /**
-     * Linear Search
+     * @param arr   the array
+     * @param value the value to search for in {@code arr}
      */
     public static int linearSearch(int[] arr, int value) {
         for (int i = 0; i < arr.length; i++) {
@@ -20,5 +21,35 @@ public class Searching {
         }
         System.out.println("The element " + value + " not found");
         return -1;
+    }
+
+    /**
+     * Binary Search.
+     *
+     * @param arr   the array
+     * @param value the value to search for in {@code arr}
+     */
+    public static int binarySearch(int[] arr, int value) {
+        int start = 0;
+        int end = arr.length - 1;
+        int middle = (start + end) / 2;
+//        System.out.println(start + " " + middle + " " + end);
+        while (arr[middle] != value && start <= end) {
+            if (value < arr[middle]) {
+                end = middle - 1;
+
+            } else {
+                start = middle + 1;
+            }
+            middle = (start + end) / 2;
+//            System.out.println(start + " " + middle + " " + end);
+        }
+        if (arr[middle] == value) {
+            System.out.println("The element is found at index: " + middle);
+            return middle;
+        } else {
+            System.out.println("The element " + value + " not found");
+            return -1;
+        }
     }
 }
