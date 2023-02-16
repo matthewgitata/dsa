d26fdfeadf52dcf8aedb5550da0868d88e7fed62
Matthew Gitata <matthewgitata@gmail.com>
2023-02-17 00:08:00 +0300

    Implement Longest Palindromic Subsequence Problem.

---
+++ Divide and Conquer Algorithms/Longest Palindromic Subsequence/src/com/matthewgitata/dsa/divideandconquer/longestpalindromicsubsequence/Main.java
@@ -0,0 +1,14 @@
+package com.matthewgitata.dsa.divideandconquer.longestpalindromicsubsequence;
+
+/**
+ * The {@code Main} class contains method which
+ * the Longest Palindromic Sequence operation is done.
+ * <p>
+ * created by @matthewgitata on 16/02/2023.
+ */
+public class Main {
+    public static void main(String[] args) {
+        LongestPalindromicSubsequence lps = new LongestPalindromicSubsequence();
+        System.out.println(lps.findLPSLength("elrmenmet"));
+    }
+}
